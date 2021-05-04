94de62dfcb6000d424a2c2c064588246fd571767
Borat <ethanbarbar@gmail.com>
2021-05-04 18:26:23 +0300

    ff

--- lecture_5/exercise_1.py
+++ lecture_5/exercise_1.py
@@ -1,17 +1,18 @@
 import random
 
-user_choice_str = input('Please enter Hetz/Pali')
-
-user_choice = 0
-if user_choice_str == 'Hetz':
-    user_choice = 1
-elif user_choice_str == 'Pali':
-    user_choice = 2
-else:
-    print('Big Problemo!')
-
-coin = random.randint(1, 2)
-if user_choice == coin:
-    print('Woho! good guess')
-else:
-    print('BAD GUESS!')
+is_user_succeeded = False
+while not is_user_succeeded:
+    user_choice_str = input('Please enter Hetz/Pali')
+    user_choice = 0
+    if user_choice_str == 'Hetz':
+        user_choice = 1
+    elif user_choice_str == 'Pali':
+        user_choice = 2
+    else:
+        print('Big Problemo!')
+    coin = random.randint(1, 2)
+    if user_choice == coin:
+        print('Woho! good guess')
+        is_user_succeeded = True
+    else:
+        print('BAD GUESS!')
